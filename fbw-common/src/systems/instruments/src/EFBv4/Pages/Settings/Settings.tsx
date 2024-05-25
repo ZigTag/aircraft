ac8088011df269bba6e2fedd2e9ef0b926142f09
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2024-05-25 14:12:11 -0400

    feat: settings about page

--- fbw-common/src/systems/instruments/src/EFBv4/Pages/Settings/Settings.tsx
+++ fbw-common/src/systems/instruments/src/EFBv4/Pages/Settings/Settings.tsx
@@ -1,12 +1,69 @@
-import { DisplayComponent, FSComponent, VNode } from '@microsoft/msfs-sdk';
+import { DisplayComponent, FSComponent, Subject, VNode } from '@microsoft/msfs-sdk';
 import { PageTitle } from '../../Components/PageTitle';
 import { t } from '../../Components/LocalizedText';
+import { PageEnum } from '../../shared/common';
+import { AbstractUIView } from '../../shared/UIVIew';
+import { Pages, Switch } from '../Pages';
+import { Button } from '../../Components/Button';
+import { SettingsAboutPage } from './SettingsAboutPage';
+import SettingsPage = PageEnum.SettingsPage;
 
-export class Settings extends DisplayComponent<any> {
+export class Settings extends AbstractUIView {
+  private readonly activePage = Subject.create<PageEnum.SettingsPage>(PageEnum.SettingsPage.Index);
+
+  private readonly activePageSetter = this.activePage.set.bind(this.activePage);
+
+  private readonly pages: Pages = [
+    [PageEnum.SettingsPage.Index, <SettingsIndex onPageSelected={this.activePageSetter} />],
+    [PageEnum.SettingsPage.SimOptions, <span />],
+    [PageEnum.SettingsPage.Realism, <span />],
+    [PageEnum.SettingsPage.ThirdPartyOptions, <span />],
+    [PageEnum.SettingsPage.AtsuAoc, <span />],
+    [PageEnum.SettingsPage.Audio, <span />],
+    [PageEnum.SettingsPage.flyPad, <span />],
+    [PageEnum.SettingsPage.About, <SettingsAboutPage />],
+  ];
+
+  resume() {
+    this.activePage.set(SettingsPage.Index);
+  }
+
   render(): VNode {
+    return <Switch ref={this.rootRef} pages={this.pages} activePage={this.activePage} />;
+  }
+}
+
+interface SettingsIndexProps {
+  onPageSelected: (page: PageEnum.SettingsPage) => void;
+}
+
+class SettingsIndex extends AbstractUIView<SettingsIndexProps> {
+  private readonly items = [
+    [PageEnum.SettingsPage.Index, t('Settings.SimOptions.Title')],
+    [PageEnum.SettingsPage.Realism, t('Settings.Realism.Title')],
+    [PageEnum.SettingsPage.ThirdPartyOptions, t('Settings.ThirdPartyOptions.Title')],
+    [PageEnum.SettingsPage.AtsuAoc, t('Settings.AtsuAoc.Title')],
+    [PageEnum.SettingsPage.Audio, t('Settings.Audio.Title')],
+    [PageEnum.SettingsPage.flyPad, t('Settings.flyPad.Title')],
+    [PageEnum.SettingsPage.About, t('Settings.About.Title')],
+  ] as const;
+
+  render(): VNode | null {
     return (
-      <div>
+      <div ref={this.rootRef}>
         <PageTitle>{t('Settings.Title')}</PageTitle>
+
+        <div class="space-y-6">
+          {this.items.map(([page, title]) => (
+            <Button
+              class="page flex justify-between rounded-md border-2 border-transparent bg-theme-accent p-6 transition duration-100 hover:border-theme-highlight"
+              onClick={() => this.props.onPageSelected(page)}
+            >
+              <p class="text-2xl">{title}</p>
+              <i class={`bi-chevron-right text-[30px] text-inherit`} />
+            </Button>
+          ))}
+        </div>
       </div>
     );
   }
---
+++ fbw-common/src/systems/instruments/src/EFBv4/Pages/Settings/SettingsAboutPage.tsx
@@ -0,0 +1,121 @@
+import { DisplayComponent, FSComponent, Subject, Subscribable, VNode } from '@microsoft/msfs-sdk';
+import { AbstractUIView } from '../../shared/UIVIew';
+import { AircraftGithubVersionChecker, BuildInfo } from '@flybywiresim/fbw-sdk';
+import { EFBSimvars } from '../../EFBSimvarPublisher';
+
+import FbwTail from '../../Assets/FBW-Tail.svg';
+
+interface CommunityPanelPlayerData {
+  bCanSignOut: boolean;
+  bDisable: boolean;
+  sAvatar: string;
+  sBuildVersion: string;
+  sMoney: string;
+  sCurrency: string;
+  sName: string;
+  sRichPresence: string;
+  sStatus: string;
+}
+
+export class SettingsAboutPage extends AbstractUIView {
+  private readonly title = Subject.create<string | null>(null);
+
+  private readonly version = Subject.create<string | null>(null);
+
+  private readonly buildInfo = Subject.create<BuildInfo | null>(null);
+
+  onAfterRender(node: VNode) {
+    super.onAfterRender(node);
+
+    if (process.env.AIRCRAFT_PROJECT_PREFIX) {
+      AircraftGithubVersionChecker.getBuildInfo(process.env.AIRCRAFT_PROJECT_PREFIX).then((info) =>
+        this.buildInfo.set(info),
+      );
+    }
+
+    this.bus
+      .getSubscriber<EFBSimvars>()
+      .on('title')
+      .whenChanged()
+      .handle((it) => this.title.set(it));
+
+    const listener = RegisterViewListener('JS_LISTENER_COMMUNITY');
+
+    listener.on('SetGamercardInfo', (data: CommunityPanelPlayerData) => {
+      this.version.set(data.sBuildVersion);
+    });
+  }
+
+  render(): VNode | null {
+    return (
+      <div ref={this.rootRef} class="pointer-events-none absolute inset-y-0 flex flex-col justify-center px-16">
+        <div class="flex flex-row items-center">
+          <div class="flex flex-col">
+            <div class="flex flex-row items-center">
+              <img class="w-[36px]" src={FbwTail} alt="" />
+              <h1 class="font-manrope ml-4 text-4xl font-bold">flyPadOS</h1>
+              <h1 class="font-manrope ml-2 text-4xl font-bold text-cyan">4</h1>
+            </div>
+
+            <p class="mt-3 text-2xl">
+              Made with love by contributors in Québec, Germany, the United States, Singapore, Indonesia, New Zealand,
+              Australia, Spain, the United Kingdom, France, the Netherlands, Sweden, and Switzerland!
+            </p>
+          </div>
+        </div>
+        <div class="mt-8 flex flex-col justify-center">
+          <p>&copy; 2020-2024 FlyByWire Simulations and its contributors, all rights reserved.</p>
+          <p>Licensed under the GNU General Public License Version 3</p>
+        </div>
+
+        <div class="mt-16">
+          <h1 class="font-bold">Build Info</h1>
+          <div class="mt-4">
+            <BuildInfoEntry title="Sim Version" value={this.version} />
+            <BuildInfoEntry title="Aircraft Version" value={this.buildInfo?.map((it) => it?.version ?? '')} />
+            <BuildInfoEntry title="Livery Title" value={this.title} />
+            <BuildInfoEntry title="Built" value={this.buildInfo?.map((it) => it?.built ?? '')} />
+            <BuildInfoEntry title="Ref" value={this.buildInfo?.map((it) => it?.ref ?? '')} />
+            <BuildInfoEntry title="SHA" value={this.buildInfo?.map((it) => it?.sha ?? '')} underline={7} />
+            <BuildInfoEntry title="Event Name" value={this.buildInfo?.map((it) => it?.eventName ?? '')} />
+            <BuildInfoEntry
+              title="Pretty Release Name"
+              value={this.buildInfo?.map((it) => it?.prettyReleaseName ?? '')}
+            />
+            {/*{sentryEnabled === SentryConsentState.Given && (*/}
+            {/*  <BuildInfoEntry title="Sentry Session ID" value={sessionId} />*/}
+            {/*)}*/}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const SPACE_BETWEEN = 28;
+
+interface BuildInfoEntryProps {
+  title: string;
+
+  value: Subscribable<string | null>;
+
+  underline?: number;
+}
+
+class BuildInfoEntry extends DisplayComponent<BuildInfoEntryProps> {
+  private readonly first = this.props.value.map((it) => it?.substring(0, this.props.underline ?? 0));
+
+  private readonly last = this.props.value.map((it) => it?.substring(this.props.underline ?? 0));
+
+  render(): VNode | null {
+    return (
+      <div class="mt-2 flex flex-row font-mono">
+        <p>{this.props.title + '\u00A0'.repeat(Math.abs(SPACE_BETWEEN - this.props.title.length))}</p>
+        <p class="ml-4">
+          <span class="text-theme-highlight underline">{this.first}</span>
+          {this.last}
+        </p>
+      </div>
+    );
+  }
+}
