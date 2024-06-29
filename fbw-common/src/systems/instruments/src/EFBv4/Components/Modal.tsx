5fa379c49dcbc0767b95a5d93f2bf1efb6b0630a
ErickTorresBrownU <erick_torres@brown.edu>
2024-06-28 21:19:19 -0500

    refactor: use Button component in Modals instead

--- fbw-common/src/systems/instruments/src/EFBv4/Components/Modal.tsx
+++ fbw-common/src/systems/instruments/src/EFBv4/Components/Modal.tsx
@@ -3,6 +3,7 @@ import { AbstractUIView } from 'instruments/src/EFBv4/shared/UIView';
 import { FSComponent } from '@microsoft/msfs-sdk';
 import { EFB_EVENT_BUS } from 'instruments/src/EFBv4/EfbV4FsInstrument';
 import { FlypadControlEvents } from 'instruments/src/EFBv4/FlypadControlEvents';
+import { Button } from './Button';
 import { t } from '../Components/LocalizedText';
 
 export enum ModalKind {
@@ -39,20 +40,15 @@ class PromptModal extends AbstractUIView<PromptModalProps> {
   private readonly declineButtonRef = FSComponent.createRef<HTMLButtonElement>();
   private readonly confirmButtonRef = FSComponent.createRef<HTMLButtonElement>();
 
-  private handleDecline(): void {
+  private handleDecline = () => {
     this.props.onReject?.();
     popModal();
-  }
+  };
 
-  private handleConfirm(): void {
+  private handleConfirm = () => {
     this.props.onConfirm?.();
     popModal();
-  }
-
-  onAfterRender(): void {
-    this.declineButtonRef.instance.addEventListener('click', this.handleDecline.bind(this));
-    this.confirmButtonRef.instance.addEventListener('click', this.handleConfirm.bind(this));
-  }
+  };
 
   render(): VNode | null {
     return (
@@ -61,20 +57,15 @@ class PromptModal extends AbstractUIView<PromptModalProps> {
         <p class="mt-4">{this.props.bodyText}</p>
 
         <div class="mt-8 flex flex-row space-x-4">
-          <button
-            ref={this.declineButtonRef}
-            type="button"
-            class="flex w-full items-center justify-center rounded-md border-2 border-theme-accent bg-theme-accent px-8 py-2 text-center text-theme-text transition duration-100 hover:border-theme-highlight hover:bg-theme-body hover:text-theme-highlight"
+          <Button
+            class="w-full border-theme-accent bg-theme-accent text-theme-text hover:border-theme-highlight"
+            onClick={this.handleDecline}
           >
             {this.props.declineText ?? t('Modals.Cancel')}
-          </button>
-          <button
-            ref={this.confirmButtonRef}
-            type="button"
-            class="flex w-full items-center justify-center rounded-md border-2 border-theme-highlight bg-theme-highlight px-8 py-2 text-center text-theme-body transition duration-100 hover:bg-theme-body hover:text-theme-highlight"
-          >
+          </Button>
+          <Button class="w-full" onClick={this.handleConfirm}>
             {this.props.confirmText ?? t('Modals.Confirm')}
-          </button>
+          </Button>
         </div>
       </div>
     );
@@ -84,27 +75,19 @@ class PromptModal extends AbstractUIView<PromptModalProps> {
 class AlertModal extends AbstractUIView<AlertModalProps> {
   private readonly acknowledgeButtonRef = FSComponent.createRef<HTMLButtonElement>();
 
-  private handleAcknowledge(): void {
+  private handleAcknowledge = () => {
     this.props.onAcknowledge?.();
     popModal();
-  }
-
-  onAfterRender(): void {
-    this.acknowledgeButtonRef.instance.addEventListener('click', this.handleAcknowledge.bind(this));
-  }
+  };
 
   render(): VNode | null {
     return (
       <div class="pointer-events-auto w-5/12 rounded-xl border-2 border-theme-accent bg-theme-body p-8">
         <h1 class="font-bold">{this.props.title}</h1>
         <p class="mt-4">{this.props.bodyText}</p>
-        <button
-          ref={this.acknowledgeButtonRef}
-          type="button"
-          class="mt-8 flex w-full items-center justify-center rounded-md border-2 border-theme-highlight bg-theme-highlight px-8 py-2 text-center text-theme-body transition duration-100 hover:bg-theme-body hover:text-theme-highlight"
-        >
+        <Button class="mt-8 w-full" onClick={this.handleAcknowledge}>
           {this.props.acknowledgeText ?? t('Modals.Okay')}
-        </button>
+        </Button>
       </div>
     );
   }
