03982df8f9566f831998d023495ff7e10b3faf56
ErickTorresBrownU <erick_torres@brown.edu>
2024-07-02 02:57:50 -0500

    add show_notification event to FlypadControlEvents

--- fbw-common/src/systems/instruments/src/EFBv4/FlypadControlEvents.ts
+++ fbw-common/src/systems/instruments/src/EFBv4/FlypadControlEvents.ts
@@ -1,7 +1,9 @@
+import { Notification } from './Components/Notification';
 import { Modal } from './Components/Modal';
 
 export interface FlypadControlEvents {
   set_tooltip: { id: string | null; shown: boolean; text: string };
   show_modal: Modal;
   pop_modal: {};
+  show_notification: Notification;
 }
