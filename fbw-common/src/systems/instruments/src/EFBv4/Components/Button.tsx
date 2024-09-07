fd8c485ccff67cb0eccf7ca4a67ce620df8a439b
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2024-09-07 14:45:25 -0400

    feat(components/button): add neutral theme

--- fbw-common/src/systems/instruments/src/EFBv4/Components/Button.tsx
+++ fbw-common/src/systems/instruments/src/EFBv4/Components/Button.tsx
@@ -5,7 +5,6 @@ import {
   ComponentProps,
   Subscribable,
   SubscribableUtils,
-  Subject,
   MappedSubject,
 } from '@microsoft/msfs-sdk';
 import { twMerge } from 'tailwind-merge';
@@ -20,6 +19,7 @@ interface ButtonProps extends ComponentProps {
 }
 
 export enum ButtonTheme {
+  Neutral,
   Highlight,
   Danger,
 }
@@ -44,6 +44,8 @@ export class Button extends DisplayComponent<ButtonProps> {
       return twMerge(
         !this.props.unstyled &&
           'flex items-center justify-center space-x-4 rounded-md border-2 border-theme-highlight bg-theme-highlight p-2 text-theme-body transition duration-100 hover:bg-theme-body hover:text-theme-highlight',
+        theme === ButtonTheme.Neutral &&
+          'border-theme-accent bg-theme-accent text-theme-text hover:border-theme-highlight hover:text-theme-highlight',
         theme === ButtonTheme.Danger && 'border-utility-red bg-utility-red text-theme-text hover:text-utility-red',
         disabled && 'pointer-events-none opacity-30',
         propClass,
